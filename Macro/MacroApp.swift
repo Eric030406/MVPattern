4852cf166de80579e330f335f298106f25964e91
Eric <real.dassy@gmail.com>
2023-09-28 18:21:25 +0900

    Initial Commit

---
+++ Macro/MacroApp.swift
@@ -0,0 +1,17 @@
+//
+//  MacroApp.swift
+//  Macro
+//
+//  Created by Eric on 9/28/23.
+//
+
+import SwiftUI
+
+@main
+struct MacroApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
